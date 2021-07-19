8257f16086e86a6222ce7f641e8768e137d68f24
SaitejareddyTileti-0293 <saitejareddytileti@gmail.com>
2021-07-19 21:59:56 +0530

    feat: add and configure tailwind for react

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sora: ['Sora', 'sans-serif']
+      }
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
