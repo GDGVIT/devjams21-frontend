eeef15a33956f5516272e38dcabc95ce68613a3c
Sricharan Ramesh <charan1952001@gmail.com>
2021-07-21 21:50:15 +0530

    feat: add routing

--- src/Pages/Faq.jsx
+++ src/Pages/Faq.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 export default function Faq () {
   return (
     <div className='grid place-content-center h-screen bg-green-200 text-xl'>
-      <div className='font-sora text-green-700'>This is the home page</div>
+      <div className='font-sora text-green-700'>This is the FAQ page</div>
       <div className='font-sora font-semibold text-green-700'>
-        This is the home page
+        This is the FAQ page
       </div>
       <div className='font-sora font-bold text-green-700'>
-        This is the home page
+        This is the FAQ page
       </div>
     </div>
   )
