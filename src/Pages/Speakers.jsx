bbc0ffdbf8d5c107a6321541252171fa731f020d
SaitejareddyTileti-0293 <saitejareddytileti@gmail.com>
2021-07-22 19:35:16 +0530

    fix: lint issues

--- src/Pages/Speakers.jsx
+++ src/Pages/Speakers.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
-import { ReactComponent as SpeakersBackground } from '../assets/Speaker Moon.svg'
-import { ReactComponent as SpeakersMap } from '../assets/Speakers Map.svg'
+import { ReactComponent as SpeakersBackground } from '../Assets/Speaker Moon.svg'
+import { ReactComponent as SpeakersMap } from '../Assets/Speakers Map.svg'
 
 export default function Speakers () {
   return (
     <div>
       <SpeakersBackground className='w-full h-auto' />
 
-      <div class='-my-3 bg-jams_black p-10'>
+      <div class='-my-6 bg-jams_black p-10'>
         <div class='font-sora font-bold text-2xl md:text-3xl md:my-6 xl:text-5xl text-center text-white'>
           Speakers
         </div>
+        <div className='text-' />
         <SpeakersMap className='w-full h-auto my-10 lg:w-2/3 mx-auto' />
       </div>
     </div>
--- src/Pages/Timeline.jsx
+++ src/Pages/Timeline.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import { ReactComponent as TimelineBackground } from '../assets/Moon Timeline.svg'
-import { ReactComponent as TimelineMap } from '../assets/Timeline Map.svg'
+import { ReactComponent as TimelineBackground } from '../Assets/Moon Timeline.svg'
+import { ReactComponent as TimelineMap } from '../Assets/Timeline Map.svg'
 
 export default function Timeline () {
   return (
     <div>
       <TimelineBackground className='w-full h-auto' />
 
-      <div class='-my-3 bg-jams_black p-10'>
+      <div class='-my-6 bg-jams_black p-10'>
         <div class='font-sora font-bold text-2xl md:text-3xl md:my-6 xl:text-5xl text-center text-white'>
           TimeLine
         </div>
