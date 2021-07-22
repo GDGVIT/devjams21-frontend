0116cd5e974857df773b9dcb2d5747d3f18ca921
Sricharan Ramesh <charan1952001@gmail.com>
2021-07-22 09:48:07 +0530

    feat: add navbar

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom' 
+import HomeNight from '../Assets/HomeNight.svg'
+import Train from '../Assets/Train.svg'
+import '../Styles/About.css'
 
 export default function About () {
   return (
-    <div className='grid place-content-center h-screen bg-green-200 text-xl'>
-      <div className='font-sora text-green-700'>This is the home page</div>
-      <div className='font-sora font-semibold text-green-700'>
-        This is the home page
-      </div>
-      <div className='font-sora font-bold text-green-700'>
-        This is the home page
+    <div className=''>
+      <div className='about-nav'>
+        <img  className='about-background' src={HomeNight} alt='background' />
+        <img className='about-train'  src={Train} alt='train' />
+        <div className='about-nav-link'>
+          <Link to='/timeline'>Timeline</Link>
+          <Link to='/speakers'>Speakers</Link>
+          <Link to='/timeline'>Sponsors</Link>
+          <Link to='/faq'>FAQ</Link>
+        </div>
       </div>
     </div>
   )
