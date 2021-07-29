4cc855f489b8f2ea599666bd050241b22158599a
Sricharan Ramesh <charan1952001@gmail.com>
2021-07-29 10:21:34 +0530

    fix: allign train

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css'
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
+import { ReactComponent as Discord } from './Assets/Discord.svg'
 import Navbar from './Components/Navbar'
 import About from './Pages/About.jsx'
 import Timeline from './Pages/Timeline'
@@ -21,6 +22,9 @@ function App () {
           <Redirect to='/' />
         </Switch>
       </BrowserRouter>
+      <div className='discord absolute bottom-8 right-8 z-30'>
+        <Discord className='' />
+      </div>
     </div>
   )
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import '../Styles/Navbar.css'
-import { ReactComponent as Discord } from '../Assets/Discord.svg';
 import { ReactComponent as Train } from '../Assets/Train.svg'
 import { ReactComponent as Background } from '../Assets/Background_3_1.svg'
 import { ReactComponent as Backdrop } from '../Assets/Foreground__Backdrop.svg'
@@ -8,16 +7,17 @@ import { ReactComponent as Foreground } from '../Assets/Foreground_3.svg'
 
 const Navbar = () => {
   return (
-    <div className='h-screen w-screen overflow-x-hidden'>
-      <Background className='h-screen absolute' />
-      <Backdrop className='absolute z-10 h-full' />
-      <Foreground className='h-full z-20 absolute' />
-      <Train className='w-96 left-1/2 top-2/3 z-20 absolute' />
-      <div className='absolute top-8 right-8 flex items-center'>
-        <button className='mr-4 px-9 py-1 rounded-md text-yellow-300 border-solid text-lg font-bold font-sora border-2 border-yellow-300'>Login</button>
-        <div className='discord'>
-          <Discord className='w-3/4' />
-        </div>
+    <div>
+      <div className='h-screen w-full relative overflow-hidden'>
+        <Background className='h-full absolute' />
+        <Backdrop className='absolute z-10 h-full' />
+        <Foreground className='h-full z-20 absolute' />
+        <Train className='train w-1/2 left-1/4 z-20 absolute' />
+      </div>
+      <div className='absolute top-10 right-8'>
+        <a href='www.google.com' className='z-40 cursor-pointer px-9 py-2 rounded-md text-yellow-300 border-solid text-lg font-bold font-sora border-2 border-yellow-300 hover:bg-yellow-300 text-transparent'>
+          Login
+        </a>
       </div>
     </div>
   )
