b5acccac05f373301119c284ae316db26a28a237
Sricharan Ramesh <charan1952001@gmail.com>
2021-07-30 13:30:09 +0530

    feat: add scroll animation on routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
+import React, { useState } from 'react'
 import './App.css'
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
-import { ReactComponent as Discord } from './Assets/Discord.svg'
 import Navbar from './Components/Navbar'
 import About from './Pages/About.jsx'
 import Timeline from './Pages/Timeline'
@@ -10,23 +10,24 @@ import Faq from './Pages/Faq'
 import Home from './Pages/Home'
 
 function App () {
+  const [bodyRender, setBodyRender] = useState(true)
+
   return (
     <div className='App'>
-      <Navbar />
       <BrowserRouter>
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/timeline' component={Timeline} />
-          <Route exact path='/sponsors' component={Sponsors} />
-          <Route exact path='/speakers' component={Speakers} />
-          <Route exact path='/faq' component={Faq} />
-          <Route exact path='/about' component={About} />
-          <Redirect to='/' />
-        </Switch>
+        <Navbar setBodyRender={setBodyRender} />
+        {  bodyRender &&
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/timeline' component={Timeline} />
+              <Route exact path='/sponsors' component={Sponsors} />
+              <Route exact path='/speakers' component={Speakers} />
+              <Route exact path='/faq' component={Faq} />
+              <Route exact path='/about' component={About} />
+              <Redirect to='/' />
+            </Switch>
+        }
       </BrowserRouter>
-      <div className='discord absolute bottom-8 right-8 z-30'>
-        <Discord className='' />
-      </div>
     </div>
   )
 }
