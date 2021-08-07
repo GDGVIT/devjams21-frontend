4023cdd4e893cc89c7f53119bb421cb25a9416d9
Sricharan Ramesh <charan1952001@gmail.com>
2021-08-07 22:46:15 +0530

    style: change navlinks position

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -11,17 +11,17 @@ import { moveIntoView } from '../Utils/Scroll'
 import '../Styles/Navbar.css'
 
 const Navbar = (props) => {
-  const [startAnimation, setStartAnimation] = useState(false)
   const location = useLocation()
   const history = useHistory()
+  const [startAnimation, setStartAnimation] = useState(false)
   const pathname = location.pathname
 
   const handleClick = (event) => {
     let route = event.target.id
+    if (route === 'home') {
+      route = ''
+    }
     if (pathname !== `/${route}`) {
-      if (route === 'home') {
-        route = '/'
-      }
       setStartAnimation(true)
       moveIntoView(props.setBodyRender, history, route, setStartAnimation)
     }
@@ -48,17 +48,17 @@ const Navbar = (props) => {
       {
         !startAnimation &&
           <div>
-            <div className='flex justify-around absolute bottom-27/100 text-white z-30 w-full'>
-              <h4 className='cursor-pointer md:text-xl' onClick={handleClick} id='home'>Home</h4>
-              <h4 className='cursor-pointer md:text-xl' onClick={handleClick} id='about'>About</h4>
-              <h4 className='cursor-pointer md:text-xl' onClick={handleClick} id='events'>Events</h4>
-              <h4 className='cursor-pointer md:text-xl' onClick={handleClick} id='faq'>FAQ</h4>
-              <h4 className='cursor-pointer md:text-xl' onClick={handleClick} id='sponsors'>Sponsors</h4>
-            </div>
-            <div
-              className='absolute top-8 right-8 cursor-pointer z-30 px-9 py-2 rounded-md text-yellow-300 border-solid text-lg font-bold font-sora border-2 border-yellow-300 2xl:text-xl hover:bg-yellow-300 hover:text-indigo-700 transition-all duration-300 ease-in-out'
-            >
-              Login
+            <div className='flex absolute items-center top-8 right-8 font-sora'>
+              <h4 className={`nav-link ${(pathname === '/') && 'nav-link-active'}`} onClick={handleClick} id='home'>Home</h4>
+              <h4 className={`nav-link ${(pathname === '/about') && 'nav-link-active'}`} onClick={handleClick} id='about'>About Us</h4>
+              <h4 className={`nav-link ${(pathname === '/events') && 'nav-link-active'}`} onClick={handleClick} id='events'>Events</h4>
+              <h4 className={`nav-link ${(pathname === '/faq') && 'nav-link-active'}`} onClick={handleClick} id='faq'>FAQ</h4>
+              <h4 className={`nav-link ${(pathname === '/sponsors') && 'nav-link-active'}`} onClick={handleClick} id='sponsors'>Sponsors</h4>
+              <div
+                className='cursor-pointer z-30 px-10 py-2 rounded-md text-white border-solid text-lg font-bold font-sora border-2 bg-red-500 border-red-500 2xl:text-xl hover:bg-white hover:text-red-500 hover:border-white transition-all duration-300 ease-in-out'
+              >
+                Login
+              </div>
             </div>
             <a href='https://discord.com' target='_blank' rel='noopener noreferrer'>
               <div
