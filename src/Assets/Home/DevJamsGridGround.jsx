0595dd61fdf0f5b2a078339e2c0fa4a2f1268b5c
Sricharan Ramesh <charan1952001@gmail.com>
2021-08-07 16:31:19 +0530

    style: theme change for home page

--- src/Assets/Home/DevJamsGridGround.jsx
+++ src/Assets/Home/DevJamsGridGround.jsx
@@ -13,7 +13,7 @@ const DevJamsGridGround = (props) => {
       <path
         d='M1454 30.5C1596 6.1 1660.83 0 1675.5 0V196H-9V124C130.167 116.167 425.3 101 492.5 103C576.5 105.5 845 68 966 56C1087 44 1276.5 61 1454 30.5Z'
         fill={theme === 'light' ? '#D0FFC5' : '#30FF00'}
-        fill-opacity={theme === 'light' ? '1' : '0.05'}
+        fillOpacity={theme === 'light' ? '1' : '0.05'}
       />
     </svg>
   )
