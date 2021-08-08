6235e509d9d5596ebff4bed0d7a7d238883857c6
Sricharan Ramesh <charan1952001@gmail.com>
2021-08-08 23:59:46 +0530

    feat: add navbar animation

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -24,6 +24,7 @@ const Navbar = (props) => {
     }
     if (pathname !== `/${route}`) {
       setStartAnimation(true)
+      setNavlinksOpen(false)
       setTimeout(() => {
         moveIntoView(props.setBodyRender, history, route, setStartAnimation)
       }, 700)
@@ -31,8 +32,8 @@ const Navbar = (props) => {
   }
 
   const handleNavbarOpen = () => {
-    console.log('click')
     setNavlinksOpen(!navlinksOpen)
+    console.log(navlinksOpen)
   }
 
   return (
@@ -53,30 +54,29 @@ const Navbar = (props) => {
         <Train className='w-1/2 left-1/4 z-20 absolute bottom-1/4 md:bottom-28/100 lg:bottom-28/100' />
       </div>
       <div>
-        <Logo className={`invisible lg:visible z-20 absolute left-8 ${startAnimation ? 'animateUp' : 'animateDown'}`} />
+        <Logo className={`invisible lg:visible z-20 absolute left-8 transition-all ease-in-out duration-500 ${startAnimation ? '-top-48' : 'top-8'}`} />
 
         {/* Navlinks mobile */}
-        <div className='z-30 bg-white w-36 h-full  lg:invisible'>
+        <div className='z-30 bg-white w-36 h-full lg:invisible'>
           {/* Hamburger icon */}
-          <div onClick={handleNavbarOpen} className={`absolute left-8 top-8 z-30 flex flex-col justify-between w-8 h-5 cursor-pointer lg:invisible ${startAnimation ? 'animateUp' : 'animateDown'}`}>
+          <div onClick={handleNavbarOpen} className={`absolute left-8 z-30 flex flex-col justify-between w-8 h-5 transition-all ease-in-out duration-500 cursor-pointer lg:invisible ${startAnimation ? '-top-48' : 'top-8'}`}>
             <span className='h-1 w-full bg-white rounded-lg' />
             <span className='h-1 w-full bg-white rounded-lg' />
             <span className='h-1 w-full bg-white rounded-lg' />
           </div>
           {/* Navlinks */}
-          {navlinksOpen &&
-            <div className='bg-white w-72 h-full flex flex-col items-center text-left text-black z-40 absolute left-0 top-0'>
-              <div className='close' />
-              <h4 className={`nav-link ${(pathname === '/') && 'nav-link-active'} mt-32 mb-10`} onClick={handleClick} id='home'>Home</h4>
-              <h4 className={`nav-link ${(pathname === '/about') && 'nav-link-active'} mb-10`} onClick={handleClick} id='about'>About Us</h4>
-              <h4 className={`nav-link ${(pathname === '/events') && 'nav-link-active'} mb-10`} onClick={handleClick} id='events'>Events</h4>
-              <h4 className={`nav-link ${(pathname === '/faq') && 'nav-link-active'} mb-10`} onClick={handleClick} id='faq'>FAQ</h4>
-              <h4 className={`nav-link ${(pathname === '/sponsors') && 'nav-link-active'} mb-10`} onClick={handleClick} id='sponsors'>Sponsors</h4>
-            </div>}
+          <div className={`bg-white w-72 h-full flex flex-col items-center text-left text-black z-30 absolute top-0 transition-all ease-in-out duration-500 ${navlinksOpen ? 'left-0' : '-left-72'}`}>
+            <div className='close' onClick={handleNavbarOpen} />
+            <h4 className={`nav-link ${(pathname === '/') && 'nav-link-active'} mt-32 mb-10`} onClick={handleClick} id='home'>Home</h4>
+            <h4 className={`nav-link ${(pathname === '/about') && 'nav-link-active'} mb-10`} onClick={handleClick} id='about'>About Us</h4>
+            <h4 className={`nav-link ${(pathname === '/events') && 'nav-link-active'} mb-10`} onClick={handleClick} id='events'>Events</h4>
+            <h4 className={`nav-link ${(pathname === '/faq') && 'nav-link-active'} mb-10`} onClick={handleClick} id='faq'>FAQ</h4>
+            <h4 className={`nav-link ${(pathname === '/sponsors') && 'nav-link-active'} mb-10`} onClick={handleClick} id='sponsors'>Sponsors</h4>
+          </div>
         </div>
 
         {/* Navlinks desktop */}
-        <div className={`flex absolute items-center right-8 font-sora ${startAnimation ? 'animateUp' : 'animateDown'}`}>
+        <div className={`flex absolute items-center right-8 font-sora transition-all ease-in-out duration-500 ${startAnimation ? '-top-48' : 'top-8'}`}>
           <div className='flex invisible text-white lg:visible'>
             <h4 className={`nav-link ${(pathname === '/') && 'nav-link-active'} mr-8`} onClick={handleClick} id='home'>Home</h4>
             <h4 className={`nav-link ${(pathname === '/about') && 'nav-link-active'} mr-8`} onClick={handleClick} id='about'>About Us</h4>
@@ -96,7 +96,7 @@ const Navbar = (props) => {
         {/* Discord button */}
         <a href='https://discord.com' target='_blank' rel='noopener noreferrer'>
           <div
-            className={`fixed items-center overflow-hidden flex w-14 h-14 z-40 hover:w-56 right-8 rounded transition-all duration-300 ease-in-out ${startAnimation ? 'animateDiscordDown' : 'animateDiscordUp'}`}
+            className={`fixed items-center overflow-hidden flex w-14 h-14 z-40 hover:w-56 right-8 rounded transition-all duration-300 ease-in-out ${startAnimation ? '-bottom-48' : 'bottom-5'}`}
           >
             <img src={discord} alt='Discord' className='h-full' />
             <span className='h-1/2 border-l-2 border-white' />
