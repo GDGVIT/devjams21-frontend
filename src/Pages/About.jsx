aba86caeb1d424ffe4af6f126c09d302ad3f5af9
SaitejareddyTileti-0293 <saitejareddytileti@gmail.com>
2021-08-10 11:20:28 +0530

    fix: lint issues

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,20 +1,20 @@
-import React from "react";
-import "../Styles/About.css";
+import React from 'react'
+import '../Styles/About.css'
 
-export default function About(props) {
+export default function About (props) {
   return (
-    <div className="z-50">
+    <div className='z-50'>
       {/* <div className="w-screen h-screen"></div> */}
-      <div className={`p-10 font-sora props.darkTheme && "about-content`}>
-        <h1 className="text-center text-4xl font-semibold mt-8 mb-12  2xl:text-5xl">
+      <div className={'p-10 font-sora props.darkTheme && "about-content'}>
+        <h1 className='text-center text-4xl font-semibold mt-8 mb-12  2xl:text-5xl'>
           About
         </h1>
-        <div className="my-24 mx-8 grid grid-cols-5">
-          <div className="col-span-5 md:col-span-4 lg:col-span-3">
-            <h2 className="text-2xl font-semibold mb-8 2xl:text-3xl">
+        <div className='my-24 mx-8 grid grid-cols-5'>
+          <div className='col-span-5 md:col-span-4 lg:col-span-3'>
+            <h2 className='text-2xl font-semibold mb-8 2xl:text-3xl'>
               DevJams'21
             </h2>
-            <p className="text-md leading-relaxed xl:text-lg 2xl:text-xl">
+            <p className='text-md leading-relaxed xl:text-lg 2xl:text-xl'>
               DevJams is one of a kind hackathon conducted by Developer Student
               Clubs VIT every year to encourage tech enthusiasts from across the
               country to develop their ideas for the future. The hackathon
@@ -27,12 +27,12 @@ export default function About(props) {
             </p>
           </div>
         </div>
-        <div className="my-36 mx-8 grid grid-cols-5">
-          <div className="col-span-5 md:col-start-3">
-            <h2 className="text-2xl text-start font-semibold mb-8 xl:text-2xl 2xl:text-3xl">
+        <div className='my-36 mx-8 grid grid-cols-5'>
+          <div className='col-span-5 md:col-start-3'>
+            <h2 className='text-2xl text-start font-semibold mb-8 xl:text-2xl 2xl:text-3xl'>
               About DSC
             </h2>
-            <p className="text-md leading-relaxed xl:text-lg 2xl:text-xl">
+            <p className='text-md leading-relaxed xl:text-lg 2xl:text-xl'>
               DSC VIT powered by Google Developers is a student community at VIT
               Vellore. We strive to bring about technological innovations among
               students and we do so by providing a platform for them to exhibit
@@ -42,12 +42,12 @@ export default function About(props) {
             </p>
           </div>
         </div>
-        <div className="my-24 mx-8 grid grid-cols-5">
-          <div className="col-span-5 md:col-span-4 lg:col-span-3">
-            <h2 className="text-2xl font-semibold mb-8 2xl:text-3xl">
+        <div className='my-24 mx-8 grid grid-cols-5'>
+          <div className='col-span-5 md:col-span-4 lg:col-span-3'>
+            <h2 className='text-2xl font-semibold mb-8 2xl:text-3xl'>
               About VIT
             </h2>
-            <p className="text-md leading-relaxed xl:text-lg 2xl:text-xl">
+            <p className='text-md leading-relaxed xl:text-lg 2xl:text-xl'>
               Vellore Institute of Technology, Vellore VIT, Vellore is one of
               the top-rated engineering colleges in India. Diversity is the
               strongest aspect of the institute and the students from various
@@ -59,5 +59,5 @@ export default function About(props) {
         </div>
       </div>
     </div>
-  );
+  )
 }
