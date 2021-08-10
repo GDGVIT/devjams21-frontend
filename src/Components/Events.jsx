e680f01adfe6ba0d3f7d1a11431cba354deb4d57
SaitejareddyTileti-0293 <saitejareddytileti@gmail.com>
2021-08-10 11:16:48 +0530

    feat: animation changes

--- src/Components/Events.jsx
+++ src/Components/Events.jsx
@@ -1,99 +1,101 @@
-import React, { useEffect, useRef } from 'react'
-import { resizing } from '../Utils/MasonryLayout'
-import events from '../Data/EventsData'
-import '../Styles/Components/Events.css'
+import React, { useEffect, useRef } from "react";
+import { resizing } from "../Utils/MasonryLayout";
+import events from "../Data/EventsData";
+import "../Styles/Components/Events.css";
+import Img from "../Assets/Train Animations/Night/CityLighthouse.svg";
 
 // Logos
-import { ReactComponent as DevJamsLogo } from '../Assets/Logos/DevJams Logo.svg'
-import { ReactComponent as KnockathonsLogo } from '../Assets/Logos/Knockathons Logo.svg'
-import { ReactComponent as DevTalksLogo } from '../Assets/Logos/DevTalks Logo.svg'
-import { ReactComponent as HexathonLogo } from '../Assets/Logos/Hexathon Logo.svg'
-import { ReactComponent as CTFLogo } from '../Assets/Logos/CTF Logo.svg'
+import { ReactComponent as DevJamsLogo } from "../Assets/Logos/DevJams Logo.svg";
+import { ReactComponent as KnockathonsLogo } from "../Assets/Logos/Knockathons Logo.svg";
+import { ReactComponent as DevTalksLogo } from "../Assets/Logos/DevTalks Logo.svg";
+import { ReactComponent as HexathonLogo } from "../Assets/Logos/Hexathon Logo.svg";
+import { ReactComponent as CTFLogo } from "../Assets/Logos/CTF Logo.svg";
 
 // Devjams Grid SVGs
-import { ReactComponent as RightArrow } from '../Assets/Home/Right Arrow.svg'
-import DevJamsGridGround from '../Assets/Home/DevJamsGridGround'
+import { ReactComponent as RightArrow } from "../Assets/Home/Right Arrow.svg";
+import DevJamsGridGround from "../Assets/Home/DevJamsGridGround";
 
-export default function Events (props) {
-  const homeContainerRef = useRef(null)
-  const theme = props.darkTheme ? 'dark' : 'light'
+export default function Events(props) {
+  const homeContainerRef = useRef(null);
+  const theme = props.darkTheme ? "dark" : "light";
 
   useEffect(() => {
-    const { resizeGridItem } = resizing()
-    const masonrify = () => resizeGridItem(homeContainerRef.current)
-    window.addEventListener('resize', masonrify)
+    const { resizeGridItem } = resizing();
+    const masonrify = () => resizeGridItem(homeContainerRef.current);
+    window.addEventListener("resize", masonrify);
 
-    masonrify()
+    masonrify();
 
     return () => {
-      window.removeEventListener('resize', masonrify)
-    }
-  }, [])
+      window.removeEventListener("resize", masonrify);
+    };
+  }, []);
 
   return (
     <div
       ref={homeContainerRef}
-      className='events-container mx-auto lg:w-3/4 xl:w-2/3'
+      className="events-container mx-auto lg:w-3/4 xl:w-2/3"
     >
       <DevJamsGrid theme={theme} />
       <CurrentEventGrid event={events.knockathon} theme={theme}>
-        <KnockathonsLogo className='w-56' />
+        <KnockathonsLogo className="w-56" />
       </CurrentEventGrid>
       <ComingSoonGrid event={events.devtalks} theme={theme}>
-        <DevTalksLogo className='w-48' />
+        <DevTalksLogo className="w-48" />
       </ComingSoonGrid>
       <ComingSoonGrid event={events.designzzz} theme={theme}>
-        <HexathonLogo className='w-40' />
+        <HexathonLogo className="w-40" />
       </ComingSoonGrid>
       <ComingSoonGrid event={events.ctf} theme={theme}>
-        <CTFLogo className='w-40' />
+        <CTFLogo className="w-40" />
       </ComingSoonGrid>
     </div>
-  )
+  );
 }
 
 // Don't remove event class its not for CSS
 
 const DevJamsGrid = (props) => {
-  const { theme } = props
+  const { theme } = props;
   return (
     <div
       className={`grid--${theme} coming-soon-grid--${theme} ${events.devjams.class}--${theme} event col-span-full relative shadow-md sm:rounded-3xl overflow-hidden`}
     >
-      <div className='flex items-center py-5 px-10 mb-20'>
+      <img src={Img} alt="this is bullshit" />
+      <div className="flex items-center py-5 px-10 mb-20">
         <div>
-          <DevJamsLogo className='w-32 sm:w-40 h-auto my-5' />
+          <DevJamsLogo className="w-32 sm:w-40 h-auto my-5" />
           <div
             className={`content--${theme} text-xs sm:text-sm md:text-base pb-6`}
           >
             {events.devjams.content}
           </div>
         </div>
-        <div className='absolute top-5 right-10 sm:static px-5'>
-          <RightArrow className='w-6 sm:w-8' />
+        <div className="absolute top-5 right-10 sm:static px-5">
+          <RightArrow className="w-6 sm:w-8" />
         </div>
       </div>
       <DevJamsGridGround />
     </div>
-  )
-}
+  );
+};
 
 const CurrentEventGrid = (props) => {
-  const { event, theme } = props
-  const { name, date, content } = event
+  const { event, theme } = props;
+  const { name, date, content } = event;
 
   return (
     <div
       className={`grid--${theme} register-grid--${theme} event relative shadow-md sm:rounded-3xl overflow-hidden`}
     >
-      <div className='content-container z-30 relative'>
+      <div className="content-container z-30 relative">
         <div>
           {props.children}
-          <div className='text-lg sm:text-xl lg:text-2xl font-bold'>{name}</div>
-          <div className='text-jams_pink'>{date}</div>
+          <div className="text-lg sm:text-xl lg:text-2xl font-bold">{name}</div>
+          <div className="text-jams_pink">{date}</div>
         </div>
         <div className={`content--${theme}`}>{content}</div>
-        <div className='text-center'>
+        <div className="text-center">
           <button className={`btn__register--${theme} grid-btn`}>
             Register
           </button>
@@ -101,59 +103,59 @@ const CurrentEventGrid = (props) => {
       </div>
 
       {/* circles */}
-      <div className='absolute z-0 top-0 left-0 right-0 bottom-0'>
+      <div className="absolute z-0 top-0 left-0 right-0 bottom-0">
         <Circles info={props.event} theme={theme} />
       </div>
     </div>
-  )
-}
+  );
+};
 
 const ComingSoonGrid = (props) => {
-  const { theme, event } = props
-  const { name, date, content } = event
+  const { theme, event } = props;
+  const { name, date, content } = event;
 
   return (
     <div
       className={`grid--${theme} coming-soon-grid--${theme} pt-8 event relative shadow-md sm:rounded-3xl overflow-hidden`}
     >
-      <div className='content-container z-30 relative'>
-        <div className='flex items-center gap-x-4'>
+      <div className="content-container z-30 relative">
+        <div className="flex items-center gap-x-4">
           {props.children}
           <div>
-            <div className='text-lg sm:text-xl lg:text-2xl font-bold'>
+            <div className="text-lg sm:text-xl lg:text-2xl font-bold">
               {name}
             </div>
-            <div className='text-jams_pink'>{date}</div>
+            <div className="text-jams_pink">{date}</div>
           </div>
         </div>
         <div className={`content--${theme}`}>{content}</div>
-        <div className='text-center'>
+        <div className="text-center">
           <div className={`btn__coming-soon--${theme} grid-btn`}>
             Coming Soon
           </div>
         </div>
       </div>
-      <div className='absolute z-0 top-0 bottom-0 right-0 left-0'>
+      <div className="absolute z-0 top-0 bottom-0 right-0 left-0">
         <Circles info={props.event} theme={theme} />
       </div>
     </div>
-  )
-}
+  );
+};
 
 const Circles = (props) => {
-  const { theme, info } = props
-  const { coords, classname } = info
+  const { theme, info } = props;
+  const { coords, classname } = info;
 
   return (
     <>
       <div
-        style={{ width: '300px', height: '300px' }}
+        style={{ width: "300px", height: "300px" }}
         className={`circle ${coords.circle1} ${classname}__circle--${theme}`}
       />
       <div
-        style={{ width: '200px', height: '200px' }}
+        style={{ width: "200px", height: "200px" }}
         className={`circle ${coords.circle2} ${classname}__circle--${theme}`}
       />
     </>
-  )
-}
+  );
+};
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,144 +1,147 @@
-import React, { useState } from 'react'
-import { useLocation } from 'react-router'
-import { ReactComponent as GDSCLogoNight } from '../Assets/Logos/GDSC Logo Night.svg'
-import discord from '../Assets/Discord.svg'
+import React, { useState } from "react";
+import { useLocation } from "react-router";
+import { ReactComponent as GDSCLogoNight } from "../Assets/Logos/GDSC Logo Night.svg";
+import discord from "../Assets/Discord.svg";
 
-import { ReactComponent as Train } from '../Assets/Train Animations/Train.svg'
+import { ReactComponent as Train } from "../Assets/Train Animations/Train.svg";
 
 // TODO: Need to replace this later
 // Light
-import { ReactComponent as MoonBg } from '../Assets/Train Animations/Night/MoonBg.svg'
-import { ReactComponent as NightCityAndLightHouse } from '../Assets/Train Animations/Night/CityLighthouse.svg'
-import { ReactComponent as GrassAndTrees } from '../Assets/Train Animations/Night/GrassAndTrees.svg'
+// import { ReactComponent as MoonBg } from '../Assets/Train Animations/Night/MoonBg.svg'
+// import { ReactComponent as NightCityAndLightHouse } from '../Assets/Train Animations/Night/CityLighthouse.svg'
+// import { ReactComponent as GrassAndTrees } from '../Assets/Train Animations/Night/GrassAndTrees.svg'
 
 // Dark
+import { ReactComponent as MoonBg } from "../Assets/Train Animations/Night/MoonBg.svg";
+import NightCityAndLightHouse from "../Assets/Train Animations/Night/CityLighthouse";
+import { ReactComponent as GrassAndTrees } from "../Assets/Train Animations/Night/GrassAndTrees.svg";
 
-import { useHistory } from 'react-router-dom'
-import { moveIntoView } from '../Utils/Scroll'
-import '../Styles/Navbar.css'
+import { useHistory } from "react-router-dom";
+import { moveIntoView } from "../Utils/Scroll";
+import "../Styles/Navbar.css";
 
 const Navbar = (props) => {
-  const location = useLocation()
-  const history = useHistory()
-  const [startAnimation, setStartAnimation] = useState(false)
-  const [navlinksOpen, setNavlinksOpen] = useState(false)
-  const pathname = location.pathname
+  const location = useLocation();
+  const history = useHistory();
+  const [startAnimation, setStartAnimation] = useState(false);
+  const [navlinksOpen, setNavlinksOpen] = useState(false);
+  const pathname = location.pathname;
 
   const handleClick = (event) => {
-    let route = event.target.id
-    if (route === 'home') {
-      route = ''
+    let route = event.target.id;
+    if (route === "home") {
+      route = "";
     }
     if (pathname !== `/${route}`) {
-      setStartAnimation(true)
-      setNavlinksOpen(false)
+      setStartAnimation(true);
+      setNavlinksOpen(false);
       setTimeout(() => {
-        moveIntoView(props.setBodyRender, history, route, setStartAnimation)
-      }, 700)
+        moveIntoView(props.setBodyRender, history, route, setStartAnimation);
+      }, 700);
     }
-  }
+  };
 
   const handleNavbarOpen = () => {
-    setNavlinksOpen(!navlinksOpen)
-    console.log(navlinksOpen)
-  }
+    setNavlinksOpen(!navlinksOpen);
+    console.log(navlinksOpen);
+  };
 
   return (
     <div>
-      <div className='h-screen w-full fixed overflow-hidden'>
+      <div className="h-screen w-screen fixed overflow-hidden">
         {props.darkTheme && ( // dark
           <div>
-            <MoonBg className='animation-bg h-full absolute' />
-            <NightCityAndLightHouse className='animation-city absolute z-10 h-full' />
-            <GrassAndTrees className='animation-grass h-full z-20 absolute' />
+            <MoonBg className="animation-bg h-full absolute" />
+            <NightCityAndLightHouse className="animation-city absolute z-10 h-full" />
+            <GrassAndTrees className="animation-grass h-full z-20 absolute" />
           </div>
         )}
         {!props.darkTheme && ( // TODO: change to light
           <div>
-            <MoonBg className='animation-bg h-full absolute' />
-            <NightCityAndLightHouse className='animation-city absolute z-10 h-full' />
-            <GrassAndTrees className='animation-grass h-full z-20 absolute' />
+            <MoonBg className="animation-bg h-full absolute" />
+            <NightCityAndLightHouse className="animation-city absolute z-10 h-full" />
+            <GrassAndTrees className="animation-grass h-full z-20 absolute" />
           </div>
         )}
-        <Train className='w-1/2 left-1/4 z-20 absolute bottom-1/4 md:bottom-28/100 lg:bottom-28/100' />
+        <Train className="w-1/2 left-1/4 z-20 absolute top-2/3 transform -translate-y-8" />
       </div>
       <div>
         {props.darkTheme && (
           <GDSCLogoNight
             className={`invisible lg:visible z-20 absolute left-8 transition-all ease-in-out duration-500 ${
-              startAnimation ? '-top-48' : 'top-8'
+              startAnimation ? "-top-48" : "top-8"
             }`}
           />
         )}
         {!props.darkTheme && ( // TODO: Need to change to light themed logo
           <GDSCLogoNight
             className={`invisible lg:visible z-20 absolute left-8 transition-all ease-in-out duration-500 ${
-              startAnimation ? '-top-48' : 'top-8'
+              startAnimation ? "-top-48" : "top-8"
             }`}
           />
         )}
         {/* Navlinks mobile */}
-        <div className='z-30 bg-white w-36 h-full lg:invisible'>
+        <div className="z-30 bg-white w-36 h-full lg:invisible">
           {/* Hamburger icon */}
           <div
             onClick={handleNavbarOpen}
             className={`absolute left-8 z-30 flex flex-col justify-between w-8 h-5 transition-all ease-in-out duration-500 cursor-pointer lg:invisible ${
-              startAnimation ? '-top-48' : 'top-8'
+              startAnimation ? "-top-48" : "top-8"
             }`}
           >
-            <span className='h-1 w-full bg-white rounded-lg' />
-            <span className='h-1 w-full bg-white rounded-lg' />
-            <span className='h-1 w-full bg-white rounded-lg' />
+            <span className="h-1 w-full bg-white rounded-lg" />
+            <span className="h-1 w-full bg-white rounded-lg" />
+            <span className="h-1 w-full bg-white rounded-lg" />
           </div>
           {/* Navlinks */}
           <div
             className={`bg-white w-72 h-full flex flex-col items-center text-left text-black z-30 absolute top-0 transition-all ease-in-out duration-500 ${
-              navlinksOpen ? 'left-0' : '-left-72'
+              navlinksOpen ? "left-0" : "-left-72"
             }`}
           >
-            <div className='close' onClick={handleNavbarOpen} />
+            <div className="close" onClick={handleNavbarOpen} />
             <h4
               className={`nav-link ${
-                pathname === '/' && 'nav-link-active'
+                pathname === "/" && "nav-link-active"
               } mt-32 mb-10`}
               onClick={handleClick}
-              id='home'
+              id="home"
             >
               Home
             </h4>
             <h4
               className={`nav-link ${
-                pathname === '/about' && 'nav-link-active'
+                pathname === "/about" && "nav-link-active"
               } mb-10`}
               onClick={handleClick}
-              id='about'
+              id="about"
             >
               About Us
             </h4>
             <h4
               className={`nav-link ${
-                pathname === '/events' && 'nav-link-active'
+                pathname === "/events" && "nav-link-active"
               } mb-10`}
               onClick={handleClick}
-              id='events'
+              id="events"
             >
               Events
             </h4>
             <h4
               className={`nav-link ${
-                pathname === '/faq' && 'nav-link-active'
+                pathname === "/faq" && "nav-link-active"
               } mb-10`}
               onClick={handleClick}
-              id='faq'
+              id="faq"
             >
               FAQ
             </h4>
             <h4
               className={`nav-link ${
-                pathname === '/sponsors' && 'nav-link-active'
+                pathname === "/sponsors" && "nav-link-active"
               } mb-10`}
               onClick={handleClick}
-              id='sponsors'
+              id="sponsors"
             >
               Sponsors
             </h4>
@@ -148,80 +151,80 @@ const Navbar = (props) => {
         {/* Navlinks desktop */}
         <div
           className={`flex absolute items-center right-8 font-sora transition-all ease-in-out duration-500 ${
-            startAnimation ? '-top-48' : 'top-8'
+            startAnimation ? "-top-48" : "top-8"
           }`}
         >
-          <div className='flex invisible text-white lg:visible'>
+          <div className="flex invisible text-white lg:visible">
             <h4
               className={`nav-link ${
-                pathname === '/' && 'nav-link-active'
+                pathname === "/" && "nav-link-active"
               } mr-8`}
               onClick={handleClick}
-              id='home'
+              id="home"
             >
               Home
             </h4>
             <h4
               className={`nav-link ${
-                pathname === '/about' && 'nav-link-active'
+                pathname === "/about" && "nav-link-active"
               } mr-8`}
               onClick={handleClick}
-              id='about'
+              id="about"
             >
               About Us
             </h4>
             <h4
               className={`nav-link ${
-                pathname === '/events' && 'nav-link-active'
+                pathname === "/events" && "nav-link-active"
               } mr-8`}
               onClick={handleClick}
-              id='events'
+              id="events"
             >
               Events
             </h4>
             <h4
               className={`nav-link ${
-                pathname === '/faq' && 'nav-link-active'
+                pathname === "/faq" && "nav-link-active"
               } mr-8`}
               onClick={handleClick}
-              id='faq'
+              id="faq"
             >
               FAQ
             </h4>
             <h4
               className={`nav-link ${
-                pathname === '/sponsors' && 'nav-link-active'
+                pathname === "/sponsors" && "nav-link-active"
               } mr-8`}
               onClick={handleClick}
-              id='sponsors'
+              id="sponsors"
             >
               Sponsors
             </h4>
           </div>
 
           {/* Login button */}
-          <div className='cursor-pointer z-30 px-10 py-2 rounded-md text-white border-solid text-lg font-bold font-sora border-2 bg-red-500 border-red-500 2xl:text-xl hover:bg-white hover:text-red-500 hover:border-white transition-all duration-300 ease-in-out'>
+          <div className="cursor-pointer z-30 px-10 py-2 rounded-md text-white border-solid text-lg font-bold font-sora border-2 bg-red-500 border-red-500 2xl:text-xl hover:bg-white hover:text-red-500 hover:border-white transition-all duration-300 ease-in-out">
             Login
           </div>
         </div>
 
         {/* Discord button */}
-        <a href='https://discord.com' target='_blank' rel='noopener noreferrer'>
+        <a href="https://discord.com" target="_blank" rel="noopener noreferrer">
           <div
             className={`fixed items-center overflow-hidden flex w-14 h-14 z-40 hover:w-56 right-8 rounded transition-all duration-300 ease-in-out ${
-              startAnimation ? '-bottom-48' : 'bottom-5'
+              startAnimation ? "-bottom-48" : "bottom-5"
             }`}
           >
-            <img src={discord} alt='Discord' className='h-full' />
-            <span className='h-1/2 border-l-2 border-white' />
-            <h1 className='discord-bg font-sora whitespace-nowrap font-semibold ml-3 text-white w-52 h-full'>
+            <img src={discord} alt="Discord" className="h-full" />
+            <span className="h-1/2 border-l-2 border-white" />
+            <h1 className="discord-bg font-sora whitespace-nowrap font-semibold ml-3 text-white w-52 h-full">
               Join our Discord
             </h1>
           </div>
         </a>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
