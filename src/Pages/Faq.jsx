8a45a3e03f494246b65e43a27e2b5aab78d71ae8
Sricharan Ramesh <charan1952001@gmail.com>
2021-08-12 21:57:10 +0530

    feat: add baner

--- src/Pages/Faq.jsx
+++ src/Pages/Faq.jsx
@@ -1,15 +1,10 @@
 import React from 'react'
+import Baner from '../Components/Baner'
 
 export default function Faq () {
   return (
-    <div className='grid place-content-center h-screen bg-green-200 text-xl'>
-      <div className='font-sora text-green-700'>This is the FAQ page</div>
-      <div className='font-sora font-semibold text-green-700'>
-        This is the FAQ page
-      </div>
-      <div className='font-sora font-bold text-green-700'>
-        This is the FAQ page
-      </div>
+    <div className='z-30'>
+      <Baner title='FAQ' color='#2BA24C' />
     </div>
   )
 }
