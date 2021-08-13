7d607b08ce66e6859031369105da4d1f36adc806
SaitejareddyTileti-0293 <saitejareddytileti@gmail.com>
2021-08-13 09:34:01 +0530

    feat: home page 4 grid layout

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import Events from '../Components/Events'
-import { ReactComponent as DevJamsLogo } from '../Assets/Logos/DevJams Logo.svg'
-import { ReactComponent as Mouse } from '../Assets/Mouse.svg'
+import React from "react";
+import Events from "../Components/Events";
+import { ReactComponent as DevJamsLogo } from "../Assets/Logos/DevJams Logo.svg";
+import { ReactComponent as Mouse } from "../Assets/Mouse.svg";
 
-export default function Home (props) {
+export default function Home(props) {
   return (
     <>
-      <DevJamsCard />
+      {/* <DevJamsCard /> */}
       <div
         className={`${
-          props.darkTheme ? 'bg-jams_black' : 'bg-white'
+          props.darkTheme ? "bg-jams_black" : "bg-white"
         } font-sora py-10`}
       >
         <div
           className={`${
-            props.darkTheme ? 'text-white' : 'text-black'
+            props.darkTheme ? "text-white" : "text-black"
           } text-2xl sm:text-3xl md:text-4xl my-5 sm:my-10 font-bold text-center`}
         >
           DevJams 2021
@@ -22,19 +22,19 @@ export default function Home (props) {
         <Events darkTheme={props.darkTheme} />
       </div>
     </>
-  )
+  );
 }
 
 const DevJamsCard = () => {
   return (
-    <div className='w-screen h-screen grid place-items-center relative bottom-20'>
+    <div className="w-screen h-screen grid place-items-center relative bottom-20">
       <div
-        style={{ background: '#181A6D' }}
-        className='card-shadow grid place-items-center p-5 z-50 rounded-lg'
+        style={{ background: "#181A6D" }}
+        className="card-shadow grid place-items-center p-5 z-50 rounded-lg"
       >
         <DevJamsLogo />
-        <Mouse className='w-5' />
+        <Mouse className="w-5" />
       </div>
     </div>
-  )
-}
+  );
+};
