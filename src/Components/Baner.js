5a4580a1a9612ac32cdab5a80b52c978c0d6fe3d
Sricharan Ramesh <charan1952001@gmail.com>
2021-08-13 15:49:10 +0530

    fix: make baner responsive

--- src/Components/Baner.js
+++ src/Components/Baner.js
@@ -1,15 +1,16 @@
 import React from 'react'
+import '../Styles/Home.css'
 import { ReactComponent as Mouse } from '../Assets/Mouse.svg'
 
 export default function Baner (props) {
   return (
-    <div className='w-screen h-screen grid place-items-center z-30 relative bottom-20'>
+    <div className='w-screen h-screen grid place-items-center relative bottom-20'>
       <div
         style={{ background: props.color }}
-        className='card-shadow grid place-items-center pt-12 px-14 pb-5 rounded-lg text-white font-circularStd text-8xl font-bold'
+        className='card-shadow grid text-white font-sora font-bold text-4xl md:text-6xl xl:text-7xl place-items-center py-5 px-9 lg:px-14 lg:pt-8 z-10 rounded-lg mx-5'
       >
         {props.title}
-        <Mouse className='w-5 text-white mt-3' />
+        <Mouse className='w-5 mouse--dark mt-5' />
       </div>
     </div>
   )
