897461016f76ed3ac3a44e777db82e2f5de07865
Sricharan Ramesh <charan1952001@gmail.com>
2021-08-26 09:44:49 +0530

    style: dark theme for faq

--- src/Pages/Sponsors.jsx
+++ src/Pages/Sponsors.jsx
@@ -3,7 +3,7 @@ import Baner from '../Components/Baner'
 
 const SponsorCard = (props) => {
   return (
-    <div className='w-24 h-24 sm:w-40 sm:h-40 lg:w-56 lg:h-56 bg-white shadow-lg rounded-2xl' />
+    <div className='w-28 h-28 sm:w-40 sm:h-40 lg:w-56 lg:h-56 bg-white shadow-lg rounded-2xl' />
   )
 }
 
@@ -20,7 +20,7 @@ export default function Sponsors () {
       <Baner color='#3B7DED' title='Sponsors' handleScroll={handleScroll} />
       <div className='flex w-screen h-screen' ref={contentRef}>
         <div className='m-auto z-20'>
-          <div className='grid gap-8 sm:gap-16 grid-cols-2 xl:grid-cols-4 sm:pb-16 md:pb-10 pt-28 md:pt-32'>
+          <div className='grid gap-8 sm:gap-16 grid-cols-2 xl:grid-cols-4 sm:pb-16 md:pb-10 pt-24 md:pt-32'>
             <SponsorCard />
             <SponsorCard />
             <SponsorCard />
