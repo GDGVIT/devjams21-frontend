f3c21bc8370848cfd977b93dcc964af32c4bfc9b
Sricharan Ramesh <charan1952001@gmail.com>
2021-08-29 15:36:23 +0530

    feat: scroll animation for timeline

--- src/Pages/Timeline.jsx
+++ src/Pages/Timeline.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Baner from '../Components/Baner'
 import Contact from '../Components/Contact'
 
 export default function Timeline () {
-  // const contentRef = useRef(null)
-  // const handleScroll = () => {
-  //   if (contentRef) {
-  //     contentRef.current.scrollIntoView({ behavior: 'smooth' })
-  //   }
-  // }
+  const contentRef = useRef(null)
+  const handleScroll = () => {
+    if (contentRef) {
+      contentRef.current.scrollIntoView({ behavior: 'smooth' })
+    }
+  }
 
   return (
     <div className='z-20 absolute'>
-      <Baner color='#F9C531' title='Timeline' />
-      <div className='w-screen p-10 grid place-items-center'>
+      <Baner color='#F9C531' title='Timeline' handleScroll={handleScroll} />
+      <div className='w-screen p-10 grid place-items-center' ref={contentRef}>
         <div className='text-3xl font-bold font-sora bg-jams_yellow text-white px-20 py-8 rounded-full'>
           Coming Soon ...
         </div>
