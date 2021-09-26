f96c4dda019a9bdb8af22cb6bf20bed3fdca179d
Sricharan Ramesh <charan1952001@gmail.com>
2021-09-26 18:22:29 +0530

    feat: add timeline

--- src/Pages/Timeline.jsx
+++ src/Pages/Timeline.jsx
@@ -1,6 +1,8 @@
 import React, { useRef } from 'react'
 import Baner from '../Components/Baner'
 import Contact from '../Components/Contact'
+import { ReactComponent as TimelineDark } from '../Assets/Timeline/Timeline Dark.svg'
+import { ReactComponent as TimelineLight } from '../Assets/Timeline/Timeline Light.svg'
 
 export default function Timeline (props) {
   const contentRef = useRef(null)
@@ -14,12 +16,16 @@ export default function Timeline (props) {
     <div className='z-20 absolute'>
       <Baner color='#F9C531' title='Timeline' handleScroll={handleScroll} />
       <div
-        className='w-screen h-screen p-6 grid place-items-center'
+        className='w-screen lg:w-5/6 mx-auto p-6'
         ref={contentRef}
       >
         <div>
-          <div className='sm:text-xl font-bold w-40 h-40 font-sora bg-jams_blue text-white sm:py-8 rounded-full grid place-items-center'>
-            Coming Soon
+          <div>
+            {
+              props.darkTheme
+                ? <TimelineDark className='w-full h-auto' />
+                : <TimelineLight className='w-full h-auto' />
+            }
           </div>
         </div>
       </div>
