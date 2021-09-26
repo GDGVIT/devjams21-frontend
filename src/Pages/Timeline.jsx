1463fea4a041f6097a3985654876e75f80d66067
Sricharan Ramesh <charan1952001@gmail.com>
2021-09-26 21:14:58 +0530

    fix: lint errors

--- src/Pages/Timeline.jsx
+++ src/Pages/Timeline.jsx
@@ -19,13 +19,11 @@ export default function Timeline (props) {
         className='w-screen flex justify-center items-center lg:w-5/6 mx-auto px-6 pt-28 pb-6'
         ref={contentRef}
       >
-          <div className='w-full'>
-            {
-              props.darkTheme
-                ? <img src={TimelineDark} alt='timeline dark' className='w-full h-full' />
-                : <img src={TimelineLight} alt='timeline light' className='w-full h-full' />
-            }
-          </div>
+        <div className='w-full'>
+          {props.darkTheme
+            ? <img src={TimelineDark} alt='timeline dark' className='w-full h-full' />
+            : <img src={TimelineLight} alt='timeline light' className='w-full h-full' />}
+        </div>
       </div>
       <Contact darkTheme={props.darkTheme} />
     </div>
