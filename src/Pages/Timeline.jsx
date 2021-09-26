0736cf760188d6bdda4b805a2c7bef3f246781d7
Sricharan Ramesh <charan1952001@gmail.com>
2021-09-26 21:13:25 +0530

    feat: add in service worker

--- src/Pages/Timeline.jsx
+++ src/Pages/Timeline.jsx
@@ -1,8 +1,8 @@
 import React, { useRef } from 'react'
 import Baner from '../Components/Baner'
 import Contact from '../Components/Contact'
-import { ReactComponent as TimelineDark } from '../Assets/Timeline/Timeline Dark.svg'
-import { ReactComponent as TimelineLight } from '../Assets/Timeline/Timeline Light.svg'
+import TimelineDark from '../Assets/Timeline/Timeline Dark.svg'
+import TimelineLight from '../Assets/Timeline/Timeline Light.svg'
 
 export default function Timeline (props) {
   const contentRef = useRef(null)
@@ -16,18 +16,16 @@ export default function Timeline (props) {
     <div className='z-20 absolute'>
       <Baner color='#F9C531' title='Timeline' handleScroll={handleScroll} />
       <div
-        className='w-screen lg:w-5/6 mx-auto p-6'
+        className='w-screen flex justify-center items-center lg:w-5/6 mx-auto px-6 pt-28 pb-6'
         ref={contentRef}
       >
-        <div>
-          <div>
+          <div className='w-full'>
             {
               props.darkTheme
-                ? <TimelineDark className='w-full h-auto' />
-                : <TimelineLight className='w-full h-auto' />
+                ? <img src={TimelineDark} alt='timeline dark' className='w-full h-full' />
+                : <img src={TimelineLight} alt='timeline light' className='w-full h-full' />
             }
           </div>
-        </div>
       </div>
       <Contact darkTheme={props.darkTheme} />
     </div>
