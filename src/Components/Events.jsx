13f53690249c0e75fbfe183727ca9be97e42db23
Sricharan Ramesh <charan1952001@gmail.com>
2021-10-02 15:24:49 +0530

    update: minor updates

--- src/Components/Events.jsx
+++ src/Components/Events.jsx
@@ -21,13 +21,13 @@ export default function Events (props) {
     <div ref={homeContainerRef} className='overflow-x-hidden'>
       <div className='cards w-screen overflow-x-auto overflow-y-hidden'>
         <div className='inline-flex gap-x-10 pb-10 px-5 lg:px-12 2xl:flex 2xl:justify-around'>
-          <CurrentEventGrid
+          <ComingSoonGrid
             event={events.ctf}
             theme={theme}
-            buttonLabel='Register'
+            buttonLabel='Registrations Closed'
           >
             <CTFLogo className='w-52' />
-          </CurrentEventGrid>
+          </ComingSoonGrid>
           <CurrentEventGrid
             event={events.knockathon}
             theme={theme}
@@ -35,10 +35,18 @@ export default function Events (props) {
           >
             <KnockathonsLogo className='w-52' />
           </CurrentEventGrid>
-          <ComingSoonGrid event={events.hexathon} theme={theme}>
+          <ComingSoonGrid
+            event={events.hexathon}
+            theme={theme}
+            buttonLabel='Coming Soon'
+          >
             <HexathonLogo className='w-36 my-3' />
           </ComingSoonGrid>
-          <ComingSoonGrid event={events.devtalks} theme={theme}>
+          <ComingSoonGrid
+            event={events.devtalks}
+            theme={theme}
+            buttonLabel='Coming Soon'
+          >
             <DevTalksLogo className='w-40' />
           </ComingSoonGrid>
         </div>
@@ -129,7 +137,7 @@ const CurrentEventGrid = (props) => {
 }
 
 const ComingSoonGrid = (props) => {
-  const { theme, event } = props
+  const { theme, event, buttonLabel } = props
   const { date, content } = event
 
   return (
@@ -148,7 +156,7 @@ const ComingSoonGrid = (props) => {
           <div
             className={`btn__coming-soon--${theme} absolte bottom-4 grid-btn`}
           >
-            Coming Soon
+            {buttonLabel}
           </div>
         </div>
       </div>
